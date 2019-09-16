b517c112795664dc9d282f11719076afc23d153d
Franco Ferraguti <franco.ferraguti@pjn.gov.ar>
2019-09-16 14:17:25 -0300

    add

--- main.go
+++ main.go
@@ -27,6 +27,10 @@ func ToString(i int) string {
 	return strconv.Itoa(i)
 }
 
+func ToString64(i int64) string {
+	return strconv.FormatInt(i, 10)
+}
+
 func ToInt(s string) int {
 	val, _ := strconv.Atoi(s)
 	return val
