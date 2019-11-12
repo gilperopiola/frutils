6722ed197cece22d338bf563141906b1809a9be0
gilperopiola <ferra.main@gmail.com>
2019-11-12 16:37:52 -0300

    read body

--- gin.go
+++ gin.go
@@ -1,6 +1,7 @@
 package frutils
 
 import (
+	"io/ioutil"
 	"strings"
 
 	"github.com/gin-gonic/gin"
@@ -17,4 +18,12 @@ func GetToken(c *gin.Context) string {
 
 func GetRequestIP(c *gin.Context) string {
 	return c.Request.RemoteAddr
+}
+
+func GetRequestBody(c *gin.Context) string {
+	body, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		return "error reading request body"
+	}
+	return string(body)
 }
