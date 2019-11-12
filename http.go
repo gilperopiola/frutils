60754dcf4c673000ba3760a190b4f4fde94df368
Franco Ferraguti <franco.ferraguti@pjn.gov.ar>
2019-11-12 13:54:33 -0300

    sendHTTPRequest

---
+++ http.go
@@ -0,0 +1,24 @@
+package frutils
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+)
+
+func SendHTTPRequest(method, url, body string) (status int, response string) {
+	request, _ := http.NewRequest(method, url, bytes.NewReader([]byte(body)))
+
+	client := &http.Client{}
+	resp, err := client.Do(request)
+	if err != nil {
+		return resp.StatusCode, err.Error()
+	}
+	defer resp.Body.Close()
+
+	if b, err := ioutil.ReadAll(resp.Body); err == nil {
+		return resp.StatusCode, string(b)
+	}
+
+	return 400, err.Error()
+}
