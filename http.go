162c653608e77e330d80c4a29541e7c6386aef7e
Franco Ferraguti <franco.ferraguti@pjn.gov.ar>
2020-03-05 09:34:45 -0300

    httpToken

--- http.go
+++ http.go
@@ -22,3 +22,22 @@ func SendHTTPRequest(method, url, body string) (status int, response string) {
 
 	return 400, err.Error()
 }
+
+func SendHTTPRequestWithToken(method, url, body, token string) (status int, response string) {
+	request, _ := http.NewRequest(method, url, bytes.NewReader([]byte(body)))
+
+	request.Header.Set("Authorization", token)
+
+	client := &http.Client{}
+	resp, err := client.Do(request)
+	if err != nil {
+		return resp.StatusCode, err.Error()
+	}
+	defer resp.Body.Close()
+
+	if b, err := ioutil.ReadAll(resp.Body); err == nil {
+		return resp.StatusCode, string(b)
+	}
+
+	return 400, err.Error()
+}
